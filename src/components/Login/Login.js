32263acdc0a7b0bce9731b660355423262c0b700
ritesh-jadhav <jadhav.ritesh0416@gmail.com>
2021-06-24 21:04:56 +0530

    validity bug fixed

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -48,31 +48,31 @@ const Login = ( props ) => {
     };
   }, []);
 
-  // useEffect( ( ) => {
-  //  const identifier = setTimeout(() =>{
-  //   console.log('useEffect ran !');
-  // },500);
-  // return () => {
-  //   console.log('clean up');
-  //   clearTimeout(identifier);
-  //   };
-  // }, [enteredEmail,enteredPassword])
+  const{ isValid: emailIsValid} = emailState;
+  const{ isValid: passwordIsValid} = passwordState;
 
+  useEffect( ( ) => {
+   const identifier = setTimeout(() =>{
+    console.log('checking form validity');
+    setFormIsValid(emailIsValid && passwordIsValid);
+  },500);
+
+  return () => {
+    console.log('clean up');
+    clearTimeout(identifier);
+    };
+  }, [emailIsValid,passwordIsValid]);
 
   const emailChangeHandler = (event) => {
     dispatchEmail({ type: "USER_INPUT", val: event.target.value });
 
-    setFormIsValid(
-      event.target.value.includes("@") && passwordState.isValid
-    );
+    // setFormIsValid(event.target.value.includes("@") && passwordState.isValid);
   };
 
   const passwordChangeHandler = (event) => {
     dispatchPassword({type : 'USER_INPUT', val: event.target.value})
 
-    setFormIsValid(
-      emailState.isValid && event.target.value.trim().length > 6
-      );
+    // setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
   };
 
   const validateEmailHandler = () => {
@@ -87,7 +87,7 @@ const Login = ( props ) => {
     event.preventDefault();
     props.onLogin(emailState.value, passwordState.value);
   };
-
+  
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
