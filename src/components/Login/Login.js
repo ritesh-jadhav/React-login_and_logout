ee10e26bfc054eacac1265dc1e485387eebd75b8
ritesh-jadhav <jadhav.ritesh0416@gmail.com>
2021-06-25 20:10:33 +0530

    context bug fixed

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer,useContext } from "react";
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from '../../store/auth-context';
 
 const emailReducer = (state, action) => {
   if (action.type ==='USER_INPUT') {
@@ -40,6 +41,8 @@ const Login = ( props ) => {
     isValid: null,
   }); 
 
+  const authCtx = useContext(AuthContext);
+
   useEffect(() => {
     console.log("EFFECT RUNNING");
 
@@ -48,31 +51,31 @@ const Login = ( props ) => {
     };
   }, []);
 
-  // useEffect( ( ) => {
-  //  const identifier = setTimeout(() =>{
-  //   console.log('useEffect ran !');
-  // },500);
-  // return () => {
-  //   console.log('clean up');
-  //   clearTimeout(identifier);
-  //   };
-  // }, [enteredEmail,enteredPassword])
+  const{ isValid: emailIsValid} = emailState;
+  const{ isValid: passwordIsValid} = passwordState;
 
+  useEffect( ( ) => {
+   const identifier = setTimeout(() =>{
+    console.log('checking form validity');
+    setFormIsValid(emailIsValid && passwordIsValid);
+  },500);
+
+  return () => {
+    console.log('clean up');
+    clearTimeout(identifier);
+    };
+  }, [emailIsValid,passwordIsValid]);
 
   const emailChangeHandler = (event) => {
     dispatchEmail({ type: "USER_INPUT", val: event.target.value });
 
-    setFormIsValid(
-      event.target.value.includes("@") && passwordState.isValid
-    );
+    // setFormIsValid(event.target.value.includes("@") && passwordState.isValid);
   };
 
   const passwordChangeHandler = (event) => {
     dispatchPassword({type : 'USER_INPUT', val: event.target.value})
 
-    setFormIsValid(
-      emailState.isValid && event.target.value.trim().length > 6
-      );
+    // setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
   };
 
   const validateEmailHandler = () => {
@@ -85,9 +88,9 @@ const Login = ( props ) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
-
+  
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
